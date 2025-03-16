70b35bb56998fd2cd7c2d53d01e1a7d459a5643c
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-03-16 15:44:24 +0300

    Added social links

--- frontend/components/Contact.jsx
+++ frontend/components/Contact.jsx
@@ -79,7 +79,7 @@ export default function Contact() {
                             toast.error(error);
                         });
                     } else {
-                        throw new Error(data.message || 'Failed to send message');
+                        throw new Error(data.message || 'Failed to send</svg> message');
                     }
                 } else {
                     setIsSubmitted(true);
@@ -211,7 +211,7 @@ export default function Contact() {
                                     }
                                 }}
                             >
-                                {['linkedin'].map((social, index) => (
+                                {['linkedin', 'facebook', 'instagram', 'x'].map((social, index) => (
                                     <motion.li
                                         key={index}
                                         className="bg-[#e6e6e6cf] dark:bg-gray-700 h-10 w-10 rounded-full flex items-center justify-center shrink-0"
@@ -222,15 +222,30 @@ export default function Contact() {
                                         whileHover={{ scale: 1.1 }}
                                         whileTap={{ scale: 0.95 }}
                                     >
-                                        <a href="/https://www.linkedin.com/in/chrispin-oguna-01b364233/" className="text-blue-500 hover:text-blue-600 transition-colors">
-                                            {/* {social === 'linkedin' && (
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill='currentColor' viewBox="0 0 24 24">
-                                                    <path d="M6.812 13.937H9.33v9.312c0 .414.335.75.75.75l4.007.001a.75.75 0 0 0 .75-.75v-9.312h2.387a.75.75 0 0 0 .744-.657l.498-4a.75.75 0 0 0-.744-.843h-2.885c.113-2.471-.435-3.202 1.172-3.202 1.088-.13 2.804.421 2.804-.75V.909a.75.75 0 0 0-.648-.743A26.926 26.926 0 0 0 15.071 0c-7.01 0-5.567 7.772-5.74 8.437H6.812a.75.75 0 0 0-.75.75v4c0 .414.336.75.75.75z" />
-                                                </svg>
-                                            )} */}
+                                        <a href={
+                                            social === 'linkedin' ? "https://www.linkedin.com/in/chrispin-oguna-01b364233/" :
+                                                social === 'facebook' ? "https://www.facebook.com/chrispin.oguna.3" :
+                                                    social === 'instagram' ? "https://www.instagram.com/ogunachrispin/?hl=en" :
+                                                        social === 'x' ? "https://x.com/chrisoguna" : "#"
+                                        } className="text-blue-500 hover:text-blue-600 transition-colors">
                                             {social === 'linkedin' && (
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill='currentColor' viewBox="0 0 24 24">
                                                     <path d="M5.372 24H.418V7.97h4.954V24zM2.892 5.79C1.295 5.79 0 4.474 0 2.895 0 1.316 1.295 0 2.892 0c1.596 0 2.892 1.316 2.892 2.895s-1.296 2.895-2.892 2.895zM24 24h-4.954v-8.2c0-1.84-.036-4.202-2.56-4.202-2.56 0-2.95 2.002-2.95 4.07V24H8.582V7.97h4.75v2.176h.07c.655-1.242 2.256-2.553 4.64-2.553 4.962 0 5.876 3.266 5.876 7.517V24z" />
+                                                </svg>
+                                            )}
+                                            {social === 'facebook' && (
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill='currentColor' viewBox="0 0 24 24">
+                                                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
+                                                </svg>
+                                            )}
+                                            {social === 'instagram' && (
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill='currentColor' viewBox="0 0 24 24">
+                                                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z" />
+                                                </svg>
+                                            )}
+                                            {social === 'x' && (
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" fill='currentColor' viewBox="0 0 24 24">
+                                                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                                                 </svg>
                                             )}
                                         </a>
--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -8,23 +8,24 @@ export default function Footer() {
                     <h6 className="text-lg text-gray-300">Get in touch:</h6>
 
                     <ul className="flex flex-wrap justify-center gap-x-8 gap-4 mt-8 mb-12">
-                        <li>
-                            <a href="https://www.facebook.com/chrispinoguna" target="_blank" rel="noopener noreferrer">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="fill-blue-600 w-8 h-8" viewBox="0 0 49.652 49.652">
-                                <path d="M24.826 0C11.137 0 0 11.137 0 24.826c0 13.688 11.137 24.826 24.826 24.826 13.688 0 24.826-11.138 24.826-24.826C49.652 11.137 38.516 0 24.826 0zM31 25.7h-4.039v14.396h-5.985V25.7h-2.845v-5.088h2.845v-3.291c0-2.357 1.12-6.04 6.04-6.04l4.435.017v4.939h-3.219c-.524 0-1.269.262-1.269 1.386v2.99h4.56z" />
-                            </svg>
-                        </a>
-                    </li>
                     <li>
-                        <a href="https://www.linkedin.com/in/chrispin-oguna/" target="_blank" rel="noopener noreferrer">
+                        <a href="https://www.linkedin.com/in/chrispin-oguna-01b364233/" target="_blank" rel="noopener noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 112.196 112.196">
                             <circle cx="56.098" cy="56.097" r="56.098" fill="#007ab9" />
                             <path fill="#fff" d="M89.616 60.611v23.128H76.207V62.161c0-5.418-1.936-9.118-6.791-9.118-3.705 0-5.906 2.491-6.878 4.903-.353.862-.444 2.059-.444 3.268v22.524h-13.41s.18-36.546 0-40.329h13.411v5.715c-.027.045-.065.089-.089.132h.089v-.132c1.782-2.742 4.96-6.662 12.085-6.662 8.822 0 15.436 5.764 15.436 18.149zm-54.96-36.642c-4.587 0-7.588 3.011-7.588 6.967 0 3.872 2.914 6.97 7.412 6.97h.087c4.677 0 7.585-3.098 7.585-6.97-.089-3.956-2.908-6.967-7.496-6.967zm-6.791 59.77H41.27v-40.33H27.865v40.33z" />
                         </svg>
                     </a>
                 </li>
-                {/* <li>
-                    <a href="https://www.instagram.com/chrispinoguna/" target="_blank" rel="noopener noreferrer">
+                        <li>
+                            <a href="https://www.facebook.com/chrispin.oguna.3" target="_blank" rel="noopener noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="fill-blue-600 w-8 h-8" viewBox="0 0 49.652 49.652">
+                                <path d="M24.826 0C11.137 0 0 11.137 0 24.826c0 13.688 11.137 24.826 24.826 24.826 13.688 0 24.826-11.138 24.826-24.826C49.652 11.137 38.516 0 24.826 0zM31 25.7h-4.039v14.396h-5.985V25.7h-2.845v-5.088h2.845v-3.291c0-2.357 1.12-6.04 6.04-6.04l4.435.017v4.939h-3.219c-.524 0-1.269.262-1.269 1.386v2.99h4.56z" />
+                            </svg>
+                        </a>
+                    </li>
+                    
+                <li>
+                    <a href="https://www.instagram.com/ogunachrispin/?hl=en" target="_blank" rel="noopener noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 152 152">
                             <linearGradient id="a" x1="22.26" x2="129.74" y1="22.26" y2="129.74" gradientUnits="userSpaceOnUse">
                                 <stop offset="0" stopColor="#fae100" />
@@ -47,13 +48,13 @@ export default function Footer() {
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.github.com/chrispinoguna" target="_blank" rel="noopener noreferrer">
+                    <a href="https://x.com/chrisoguna" target="_blank" rel="noopener noreferrer">
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8" viewBox="0 0 1227 1227">
                             <path d="M613.5 0C274.685 0 0 274.685 0 613.5S274.685 1227 613.5 1227 1227 952.315 1227 613.5 952.315 0 613.5 0z" />
                             <path fill="#fff" d="m680.617 557.98 262.632-305.288h-62.235L652.97 517.77 470.833 252.692H260.759l275.427 400.844-275.427 320.142h62.239l240.82-279.931 192.35 279.931h210.074L680.601 557.98zM345.423 299.545h95.595l440.024 629.411h-95.595z" />
                         </svg>
                     </a>
-                </li> */}
+                </li>
             </ul>
 
             <p className="text-base text-gray-300">© {new Date().getFullYear()} Chrispin Oguna. All rights reserved.</p>
